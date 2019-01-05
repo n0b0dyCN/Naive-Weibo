073b17b9c74d6d5ab11a8eafaafffa5c48c5e73b
n0b0dy <n0b0dypku@gmail.com>
2019-01-05 15:41:59 +0000

    Retab file

--- graph_db/weibo/create_graph.py
+++ graph_db/weibo/create_graph.py
@@ -3,81 +3,85 @@ from config import mysql_conf, gStore_conf, path
 
 
 def create_graph():
-	
-	import pymysql as mdb	
+    
+    import pymysql as mdb    
 
-	import rdflib
-	from rdflib import URIRef, Literal, Namespace, Graph
-	from rdflib.namespace import NamespaceManager
+    import rdflib
+    from rdflib import URIRef, Literal, Namespace, Graph
+    from rdflib.namespace import NamespaceManager
 
-	con = mdb.connect(mysql_conf['host'], mysql_conf['username'], mysql_conf['password'], mysql_conf['db'])
-	conn = con.cursor(mdb.cursors.DictCursor)
+    con = mdb.connect(mysql_conf['host'], mysql_conf['username'], mysql_conf['password'], mysql_conf['db'])
+    conn = con.cursor(mdb.cursors.DictCursor)
 
-	g = Graph()
-	namespace_manager = NamespaceManager(g)
+    g = Graph()
+    namespace_manager = NamespaceManager(g)
 
-	RDF = Namespace('rdf:')
-	namespace_manager.bind('RDF', RDF, override=False)
+    RDF = Namespace('rdf:')
+    namespace_manager.bind('RDF', RDF, override=False)
 
-	FOAF = Namespace('foaf:')
-	namespace_manager.bind('foaf', FOAF, override=False)
+    FOAF = Namespace('foaf:')
+    namespace_manager.bind('foaf', FOAF, override=False)
 
-	WB = Namespace('wb:')
-	namespace_manager.bind('wb:', WB, override=False)
-
-
-	conn.execute("SELECT * FROM user;")
-	for user in conn.fetchall():
-
-		uid = URIRef(user['uid'])
-		g.add( (uid, RDF.type, FOAF.Person) )
-
-		for k, v in user.items():
-			if k in ['uid']: continue
-			g.add( (uid, FOAF[k], Literal(v)) )
+    WB = Namespace('wb:')
+    namespace_manager.bind('wb:', WB, override=False)
 
 
-	conn.execute("SELECT * FROM userrelation;")
-	for user_rel in conn.fetchall():
+    print("Dump users")
+    conn.execute("SELECT * FROM user;")
+    for user in conn.fetchall():
 
-		suid = URIRef(user_rel['suid'])
-		tuid = URIRef(user_rel['tuid'])
-		g.add( (suid, FOAF.knows, tuid) )
+        uid = URIRef(user['uid'])
+        g.add( (uid, RDF.type, FOAF.Person) )
 
-
-	conn.execute("SELECT * FROM weibo;")
-	for weibo in conn.fetchall():
-
-		uid = URIRef(weibo['uid'])
-		mid = URIRef(weibo['mid'])
-		g.add( (mid, RDF.type, WB.Post) )
-		g.add( (uid, FOAF.posted, mid) )
-
-		for k, v in weibo.items():
-			if k in ['uid', 'mid']: continue
-			g.add( (mid, WB[k], Literal(v)) )
+        for k, v in user.items():
+            if k in ['uid']: continue
+            g.add( (uid, FOAF[k], Literal(v)) )
 
 
-	conn.execute("SELECT * FROM weiborelation;")
-	for weibo_rel in conn.fetchall():
+    print("Dump user relations")
+    conn.execute("SELECT * FROM userrelation;")
+    for user_rel in conn.fetchall():
 
-		smid = URIRef(weibo_rel['smid'])
-		tmid = URIRef(weibo_rel['tmid'])
-		g.add( (smid, WB.shared, tmid) )
+        suid = URIRef(user_rel['suid'])
+        tuid = URIRef(user_rel['tuid'])
+        g.add( (suid, FOAF.knows, tuid) )
 
-	g.serialize(destination=os.path.join(path, "data/weibo.nt"), format='nt')
+
+    print("Dump weibo")
+    conn.execute("SELECT * FROM weibo;")
+    for weibo in conn.fetchall():
+
+        uid = URIRef(weibo['uid'])
+        mid = URIRef(weibo['mid'])
+        g.add( (mid, RDF.type, WB.Post) )
+        g.add( (uid, FOAF.posted, mid) )
+
+        for k, v in weibo.items():
+            if k in ['uid', 'mid']: continue
+            g.add( (mid, WB[k], Literal(v)) )
+
+
+    print("Dump weibo relations")
+    conn.execute("SELECT * FROM weiborelation;")
+    for weibo_rel in conn.fetchall():
+
+        smid = URIRef(weibo_rel['smid'])
+        tmid = URIRef(weibo_rel['tmid'])
+        g.add( (smid, WB.shared, tmid) )
+
+    g.serialize(destination=os.path.join(path, "data/weibo.nt"), format='nt')
 
 
 def build_graph():
 
-	import os
-	from GstoreConnector import GstoreConnector
+    import os
+    from GstoreConnector import GstoreConnector
 
-	# before you run this example, make sure that you have started up ghttp service (using bin/ghttp db_name port)
-	gc =  GstoreConnector(ip=gStore_conf['host'], port=gStore_conf['port'])
+    # before you run this example, make sure that you have started up ghttp service (using bin/ghttp db_name port)
+    gc =  GstoreConnector(ip=gStore_conf['host'], port=gStore_conf['port'])
 
-	gc.drop(gStore_conf['username'], gStore_conf['password'], gStore_conf['db'])
-	gc.build(gStore_conf['db'], os.path.join(path, "data/weibo.nt"), gStore_conf['username'], gStore_conf['password'])
+    gc.drop(gStore_conf['username'], gStore_conf['password'], gStore_conf['db'])
+    gc.build(gStore_conf['db'], os.path.join(path, "data/weibo.nt"), gStore_conf['username'], gStore_conf['password'])
 
-# create_graph()
-# build_graph()
+create_graph()
+build_graph()
